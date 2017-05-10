69e9c2a3faec117f68a5073aa65f7f8e53be8d67
OliCyrus <olicurys@gmail.com>
2017-05-10 23:58:27 +0400

    project start

---
+++ Network1/DBHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.Windows;
+using System.IO;
+
+namespace Network1
+{
+    class DBHelper
+    {
+        public static string ConnectionString { get; private set; }
+        public static string DbFile { get; private set; }
+
+       
+
+        public static void Create_Db_File(string fileName)
+        {
+            ConnectionString = String.Format(@"Data Source=|DataDirectory|\{0};Persist Security Info=True", fileName);
+            DbFile = fileName;
+        }
+
+        public static void Create_DB()
+        {
+            var engine = new SqlCeEngine(ConnectionString);
+            engine.CreateDatabase();
+
+            SqlCeConnection conn = null;
+            
+            try
+            {
+                conn = CreateConnection();
+                conn.Open();
+
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "Create Table Employee(" +
+                                  "ID int identity(1,1) not null primary key," +
+                                  "NAME nvarchar(100) not null)";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Can't create db.", ex);
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+                MessageBox.Show("База данных успешно создана!");
+            }
+        }
+
+        public static SqlCeConnection CreateConnection()
+        {
+            return new SqlCeConnection(ConnectionString);
+        }
+
+        public static bool IsDbFileExist
+        {
+            get { return File.Exists(DbFile); }
+        }
+    }
+}
---
+++ Network1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Network1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DBHelper.Create_Db_File("1.sdf");
+                DBHelper.Create_DB();
+                
+            }
+            catch
+            {
+                MessageBox.Show("PIZDEC!");
+            }
+            if (!DBHelper.IsDbFileExist)
+                MessageBox.Show("pizdec!");
+        }
+    }
+}
