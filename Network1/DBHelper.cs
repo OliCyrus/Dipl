07e7bac4cc809565d9ff81a5a1978771b902def5
OliCyrus <olicurys@gmail.com>
2017-05-21 21:10:00 +0400

    db complete commit

--- Network1/DBHelper.cs
+++ Network1/DBHelper.cs
@@ -15,8 +15,11 @@ namespace Network1
     {
         public static string ConnectionString { get; private set; }
         public static string DbFile { get; private set; }
+        public static string TasksSelect = "select ID[ID], Name[Имя], Start_date[Дата начала], Finish_date[Дата окончания] from Tasks ";
+        public static string EmployeeSelect = "select ID[ID], Name[Имя] from Employee ";
+        public static string AssingmentSelect = "Select a.ID[ID], t.Name[Задача], e.Name[Работник] from Assingment a, Tasks t, Employee e where a.ID_Task = T.ID and a.ID_Employee = e.ID";
 
-       
+
 
         public static void Create_Db_File(string fileName)
         {
@@ -44,12 +47,14 @@ namespace Network1
                 cmd.ExecuteNonQuery();
 
                 cmd.CommandText = "Create Table Tasks(" +
-                                  "ID int identity(1000,1) not null primary key," +
-                                  "Name nvarchar(100) not null)";
+                                  "ID int identity(1,1) not null primary key," +
+                                  "Name nvarchar(100) not null," +
+                                  "Start_date datetime not null," +
+                                  "Finish_date datetime not null)";
                 cmd.ExecuteNonQuery();
 
                 cmd.CommandText = "Create Table Assingment(" +
-                                  "ID int identity(10000,1) not null primary key," +
+                                  "ID int identity(1,1) not null primary key," +
                                   "ID_Task int not null," +
                                   "foreign key(ID_Task) references Tasks(ID)," +
                                   "ID_Employee int not null," +
@@ -99,9 +104,60 @@ namespace Network1
             }
         }
 
-        public static void add_task(TextBox txt)
+        public static string[] GetDataString(string selectCommand, string Column)
         {
-            if (txt.Text.Length != 0)
+            var t = new DataTable();
+
+            string[] result;
+
+            //if (!DbProvider.IsDbFileExist)
+            //    DbProvider.CreateDb();
+
+            using (var c = DBHelper.CreateConnection())
+            {
+                c.Open();
+                using (var a = new SqlCeDataAdapter(selectCommand, c))
+                {
+                    a.Fill(t);
+                    result = new string[t.Rows.Count];
+                }
+                for (int i = 0; i < t.Rows.Count; i++)
+                {
+                    result[i] = t.Rows[i].Field<string>(Column);
+                }
+            }
+            return result;
+        }
+
+        public static string[] GetDataInt(string selectCommand, string Column)
+        {
+            var t = new DataTable();
+
+            string[] result;
+
+            //if (!DbProvider.IsDbFileExist)
+               // DbProvider.CreateDb();
+
+            using (var c = DBHelper.CreateConnection())
+            {
+                c.Open();
+                using (var a = new SqlCeDataAdapter(selectCommand, c))
+                {
+                    a.Fill(t);
+                    result = new string[t.Rows.Count];
+                }
+                for (int i = 0; i < t.Rows.Count; i++)
+                {
+                    result[i] = t.Rows[i].Field<int>(Column).ToString();
+                }
+            }
+            return result;
+        }
+
+        ///КОД МАНИПУЛЯЦИИ С ТАБЛИЦЕЙ ЗАДАНИЙ///
+        public static void add_task(TextBox task_name, DatePicker start_date, DatePicker finish_date)
+        {
+            if (task_name.Text.Length != 0 && start_date.Text.Length != 0 && finish_date.Text.Length != 0)
             {
                 SqlCeConnection conn = null;
 
@@ -112,10 +168,14 @@ namespace Network1
                     conn.Open();
 
                     SqlCeCommand command = conn.CreateCommand();
-                    command.CommandText = "INSERT INTO Tasks(Name) "
-                    + "VALUES(@Name)";
+                    command.CommandText = "INSERT INTO Tasks(Name, Start_date, Finish_date) "
+                    + "VALUES(@Name,@Start_date, @Finish_date)";
                     command.Parameters.Add("@Name", SqlDbType.NVarChar, 100);
-                    command.Parameters["@Name"].Value = txt.Text;
+                    command.Parameters["@Name"].Value = task_name.Text;
+                    command.Parameters.Add("@Start_date", SqlDbType.DateTime);
+                    command.Parameters["@Start_date"].Value = start_date.Text;
+                    command.Parameters.Add("@Finish_date", SqlDbType.DateTime);
+                    command.Parameters["@Finish_date"].Value = finish_date.Text;
                     command.ExecuteScalar();
                     command.Parameters.Clear();
                 }
@@ -132,5 +192,241 @@ namespace Network1
             else
                 MessageBox.Show("Все поля должны быть заполнены!");
         }
+
+        public static void get_tasks_fields(DataGrid dgr, TextBox task_name , DatePicker start_date, DatePicker finish_date)
+        {
+            DataRowView drv = (DataRowView)dgr.SelectedItem;
+            task_name.Text = (drv[1]).ToString();
+            start_date.Text = (drv[2]).ToString();
+            finish_date.Text = (drv[3]).ToString();
+        }
+
+        public static void update_task(DataGrid dgr, TextBox task_name, DatePicker start_date, DatePicker finish_date)
+        {
+            if (task_name.Text.Length != 0 && start_date.Text.Length != 0 && finish_date.Text.Length != 0)
+            {
+                DataRowView drv = (DataRowView)dgr.SelectedItem;
+                SqlCeConnection conn = null;
+
+                try
+                {
+                    conn = new SqlCeConnection(DBHelper.ConnectionString);
+
+                    conn.Open();
+
+                    SqlCeCommand command = conn.CreateCommand();
+                    command.CommandText = "Update Tasks set Name = @Name, Start_date = @Start_date, Finish_date = @Finish_date where ID = @ID";
+                    command.Parameters.Add("@ID", SqlDbType.Int);
+                    command.Parameters["@ID"].Value = (drv[0]).ToString();
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 100);
+                    command.Parameters["@Name"].Value = task_name.Text;
+                    command.Parameters.Add("@Start_date", SqlDbType.DateTime);
+                    command.Parameters["@Start_date"].Value = start_date.Text;
+                    command.Parameters.Add("@Finish_date", SqlDbType.DateTime);
+                    command.Parameters["@Finish_date"].Value = finish_date.Text;
+                    command.ExecuteScalar();
+                    command.Parameters.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Данный объект уже существует!");
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+            }
+            else
+                MessageBox.Show("Все поля должны быть заполнены!");
+        }
+
+
+        ///КОД МАНИПУЛЯЦИИ С ТАБЛИЦЕЙ РАБОТНИКОВ///
+        public static void add_employee(TextBox employee_name)
+        {
+            if (employee_name.Text.Length != 0)
+            {
+                SqlCeConnection conn = null;
+
+                try
+                {
+                    conn = new SqlCeConnection(DBHelper.ConnectionString);
+
+                    conn.Open();
+
+                    SqlCeCommand command = conn.CreateCommand();
+                    command.CommandText = "INSERT INTO Employee(Name) "
+                    + "VALUES(@Name)";
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 100);
+                    command.Parameters["@Name"].Value = employee_name.Text;
+                    command.ExecuteScalar();
+                    command.Parameters.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Данный объект уже существует!");
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+            }
+            else
+                MessageBox.Show("Все поля должны быть заполнены!");
+        }
+
+        public static void update_employee(DataGrid dgr, TextBox employee_name)
+        {
+            if (employee_name.Text.Length != 0)
+            {
+                DataRowView drv = (DataRowView)dgr.SelectedItem;
+                SqlCeConnection conn = null;
+
+                try
+                {
+                    conn = new SqlCeConnection(DBHelper.ConnectionString);
+
+                    conn.Open();
+
+                    SqlCeCommand command = conn.CreateCommand();
+                    command.CommandText = "Update Employee set Name = @Name where ID = @ID";
+                    command.Parameters.Add("@ID", SqlDbType.Int);
+                    command.Parameters["@ID"].Value = (drv[0]).ToString();
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 100);
+                    command.Parameters["@Name"].Value = employee_name.Text;
+                    command.ExecuteScalar();
+                    command.Parameters.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Данный объект уже существует!");
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+
+                }
+            }
+            else
+                MessageBox.Show("Все поля должны быть заполнены!");
+        }
+
+        public static void get_emoloyee_fields(DataGrid dgr, TextBox employee_name)
+        {
+            DataRowView drv = (DataRowView)dgr.SelectedItem;
+            employee_name.Text = (drv[1]).ToString();
+        }
+
+        ///МАНИПУЛЯЦИИ С ТАБЛИЦЕЙ НАЗНАЧЕНИЙ///
+        public static void add_assingment(ComboBox task_name, ComboBox employee_name)
+        {
+            if (employee_name.Text.Length != 0 && task_name.Text.Length!=0)
+            {
+                SqlCeConnection conn = null;
+
+                try
+                {
+                    conn = new SqlCeConnection(DBHelper.ConnectionString);
+
+                    conn.Open();
+
+                    SqlCeCommand command = conn.CreateCommand();
+                    command.CommandText = "INSERT INTO Assingment(ID_Task, ID_Employee) "
+                    + "VALUES(@ID_Task, @ID_Employee)";
+                    command.Parameters.Add("@ID_Task", SqlDbType.Int);
+                    command.Parameters["@ID_Task"].Value = Convert.ToInt16(DBHelper.GetDataInt("select ID from Tasks where Name = '" + task_name.Text + "'","ID")[0]);
+                    command.Parameters.Add("@ID_Employee", SqlDbType.Int);
+                    command.Parameters["@ID_Employee"].Value = Convert.ToInt16(DBHelper.GetDataInt("select ID from Employee where Name = '" + employee_name.Text + "'", "ID")[0]);
+                    command.ExecuteScalar();
+                    command.Parameters.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Данный объект уже существует!");
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+            }
+            else
+                MessageBox.Show("Все поля должны быть заполнены!");
+        }
+
+        public static void update_assingment(DataGrid dgr, ComboBox task_name, ComboBox employee_name)
+        {
+            if (employee_name.Text.Length != 0 && task_name.Text.Length != 0)
+            {
+                DataRowView drv = (DataRowView)dgr.SelectedItem;
+                SqlCeConnection conn = null;
+
+                try
+                {
+                    conn = new SqlCeConnection(DBHelper.ConnectionString);
+
+                    conn.Open();
+
+                    SqlCeCommand command = conn.CreateCommand();
+                    command.CommandText = "Update Assingment set ID_Task = @ID_Task, ID_Employee = @ID_Employee where ID = @ID ";
+                    command.Parameters.Add("@ID_Task", SqlDbType.Int);
+                    command.Parameters["@ID_Task"].Value = Convert.ToInt16(DBHelper.GetDataInt("select ID from Tasks where Name = '" + task_name.Text + "'", "ID")[0]);
+                    command.Parameters.Add("@ID_Employee", SqlDbType.Int);
+                    command.Parameters["@ID_Employee"].Value = Convert.ToInt16(DBHelper.GetDataInt("select ID from Employee where Name = '" + employee_name.Text + "'", "ID")[0]);
+                    command.Parameters.Add("@ID", SqlDbType.Int);
+                    command.Parameters["@ID"].Value = (drv[0]).ToString();
+                    command.ExecuteScalar();
+                    command.Parameters.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Данный объект уже существует!");
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+            }
+            else
+                MessageBox.Show("Все поля должны быть заполнены!");
+        }
+
+        public static void get_assingment_fields(DataGrid dgr, ComboBox task_name, ComboBox employee_name)
+        {
+            DataRowView drv = (DataRowView)dgr.SelectedItem;
+            task_name.Text = (drv[1]).ToString();
+            employee_name.Text = (drv[2]).ToString();
+        }
+
+
+        public static void delete_raw(string TableName, DataGrid dgr)
+        {
+            DataRowView drv = (DataRowView)dgr.SelectedItem;
+            String result = (drv[0]).ToString();
+            SqlCeConnection conn = null;
+            try
+            {
+                conn = new SqlCeConnection(DBHelper.ConnectionString);
+                conn.Open();
+                SqlCeCommand command = conn.CreateCommand();
+                command.CommandText = "DELETE from " + TableName + " where ID = " + result;
+                command.ExecuteScalar();
+            }
+            catch
+            {
+                MessageBox.Show("Выделите строку для удаления!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+            }
+        }
+
+
     }
 }
--- Network1/MainWindow.xaml.cs
+++ Network1/MainWindow.xaml.cs
@@ -25,31 +25,284 @@ namespace Network1
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool IsUpdatingTasks = false;
+        bool IsUpdatingEmp_Ass = false;
+        bool IsUpdatingAssingment = false;
         public MainWindow()
         {
             InitializeComponent();
             this.Title = "Проект " + DBHelper.DbFile.ToString();
-            DBHelper.FillData("select ID[ID], Name[NAME] from Tasks ", Tasks_View);
-
-
+            DBHelper.FillData(DBHelper.TasksSelect, Tasks_View);
+            DBHelper.FillData(DBHelper.EmployeeSelect, Employee_View);
+            DBHelper.FillData(DBHelper.AssingmentSelect, Assingment_View);
         }
 
         private void Exit_Menu_Item_Click(object sender, RoutedEventArgs e)
-        { 
+        {
             Start_Window str = new Start_Window();
             str.Show();
             this.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Add_Task_Button_Click(object sender, RoutedEventArgs e)   //добавление или изменение задания
         {
-            DBHelper.FillData("select ID[ID], Name[NAME] from Tasks ", Tasks_View);
+            if (IsUpdatingTasks)
+            {
+                DBHelper.update_task(Tasks_View, Task_Name, Start_date_picker, Finish_date_picker);
+                IsUpdatingTasks = false;
+            }
+            else
+            {
+                DBHelper.add_task(Task_Name, Start_date_picker, Finish_date_picker);
+            }
+            clear_task_fields();  //очистка полей
+            unable_task_buttons();     // разблокирование кнопок, если происходило обновление
+            DBHelper.FillData(DBHelper.TasksSelect, Tasks_View);
         }
 
-        private void Add_Task_Button_Click(object sender, RoutedEventArgs e)   //добавление задания
+        private void Delete_Task_Button_Click(object sender, RoutedEventArgs e) //удаление задания
         {
-            DBHelper.add_task(Task_Name);
-            DBHelper.FillData("select ID[ID], Name[NAME] from Tasks ", Tasks_View);
+            DBHelper.delete_raw("Tasks", Tasks_View);
+            DBHelper.FillData(DBHelper.TasksSelect, Tasks_View);
+        }
+
+        private void Update_tasks_list_button_Click(object sender, RoutedEventArgs e)
+        {
+            DBHelper.FillData(DBHelper.TasksSelect, Tasks_View);
+        }
+
+        private void Tasks_View_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsUpdatingTasks)
+            {
+                DBHelper.get_tasks_fields(Tasks_View, Task_Name, Start_date_picker, Finish_date_picker);
+            }
+
+        }
+
+        private void Update_Task_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsUpdatingTasks)
+            {
+                IsUpdatingTasks = false;
+                Update_Task_Button.Content = "Изменить";
+                unable_task_buttons();
+                clear_task_fields();
+            }
+            else
+            {
+                IsUpdatingTasks = true;
+                Update_Task_Button.Content = "Отмена";
+                unable_task_buttons();
+            }
+
+
+        }
+
+        private void unable_task_buttons()
+        {
+            if (IsUpdatingTasks)
+            {
+                Delete_Task_Button.IsEnabled = false;
+                Update_tasks_list_button.IsEnabled = false;
+            }
+            else if (!IsUpdatingTasks)
+            {
+                Delete_Task_Button.IsEnabled = true;
+                Update_tasks_list_button.IsEnabled = true;
+            }
+
+        }
+
+        private void unable_buttons()
+        {
+            //switch(IsUpdatingEmp_Ass)
+            //{
+            //    case true:
+            //        Delete_button.IsEnabled = false;
+            //        Update_list_button.IsEnabled = false;
+            //        break;
+            //    case false:
+            //        Delete_button.IsEnabled = true;
+            //        Update_list_button.IsEnabled = true;
+            //        break;
+            //}
+
+            //switch(IsUpdatingAssingment)
+            //{
+            //    case true:
+
+            //}
+
+            if (IsUpdatingAssingment || IsUpdatingEmp_Ass)
+            {
+                Delete_button.IsEnabled = false;
+                Update_list_button.IsEnabled = false;
+            }
+            else
+            {
+                Delete_button.IsEnabled = true;
+                Update_list_button.IsEnabled = true;
+            }
+        }
+
+        private void clear_task_fields()
+        {
+            Task_Name.Clear();
+            Start_date_picker.Text = "";
+            Finish_date_picker.Text = "";
+        }
+
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Data_Tab_Control.SelectedIndex == 1)
+            {
+                if (IsUpdatingEmp_Ass)
+                {
+                    DBHelper.update_employee(Employee_View, Employee_Name_Textbox);
+                    Employee_Name_Textbox.Clear();
+                    IsUpdatingEmp_Ass = false;
+                    Update_button.Content = "Изменить";
+                    unable_buttons();
+                    DBHelper.FillData(DBHelper.EmployeeSelect, Employee_View);
+                }
+                else
+                {
+                    DBHelper.add_employee(Employee_Name_Textbox);
+                    DBHelper.FillData(DBHelper.EmployeeSelect, Employee_View);
+                    Employee_Name_Textbox.Clear();
+                }
+
+
+            }
+
+            else if (Data_Tab_Control.SelectedIndex == 0)
+            {
+                if (IsUpdatingAssingment)
+                {
+                    DBHelper.update_assingment(Assingment_View, Tasks_Combo_Box, Empl_Combo_Box);
+                    Tasks_Combo_Box.Text = "";
+                    Empl_Combo_Box.Text = "";
+                    IsUpdatingAssingment = false;
+                    Update_button.Content = "Изменить";
+                    unable_buttons();
+                    DBHelper.FillData(DBHelper.AssingmentSelect, Assingment_View);
+
+                }
+                else
+                {
+                    DBHelper.add_assingment(Tasks_Combo_Box, Empl_Combo_Box);
+                    DBHelper.FillData(DBHelper.AssingmentSelect, Assingment_View);
+                    Tasks_Combo_Box.Text = "";
+                    Empl_Combo_Box.Text = "";
+                }
+
+            }
+
+        }
+
+        private void Delete_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Data_Tab_Control.SelectedIndex == 1)
+            {
+                DBHelper.delete_raw("Employee", Employee_View);
+                DBHelper.FillData(DBHelper.EmployeeSelect, Employee_View);
+            }
+            else if (Data_Tab_Control.SelectedIndex == 0)
+            {
+                DBHelper.delete_raw("Assingment", Assingment_View);
+                DBHelper.FillData(DBHelper.AssingmentSelect, Assingment_View);
+            }
+        }
+
+        private void Update_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Data_Tab_Control.SelectedIndex == 1)
+            {
+                if (IsUpdatingEmp_Ass)
+                {
+                    IsUpdatingEmp_Ass = false;
+                    Update_button.Content = "Изменить";
+                    Employee_Name_Textbox.Clear();
+                    unable_buttons();
+                }
+                else if (!IsUpdatingEmp_Ass)
+                {
+                    IsUpdatingEmp_Ass = true;
+                    Update_button.Content = "Отмена";
+                    unable_buttons();
+                }
+            }
+
+            else if (Data_Tab_Control.SelectedIndex == 0)
+            {
+                if (IsUpdatingAssingment)
+                {
+                    IsUpdatingAssingment = false;
+                    Update_button.Content = "Изменить";
+                    Tasks_Combo_Box.Text = "";
+                    Empl_Combo_Box.Text = "";
+                    unable_buttons();
+
+                }
+                else if (!IsUpdatingAssingment)
+                {
+                    IsUpdatingAssingment = true;
+                    Update_button.Content = "Отмена";
+                    unable_buttons();
+                }
+            }
+
+        }
+
+        private void Update_list_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Data_Tab_Control.SelectedIndex == 0)
+            {
+                DBHelper.FillData(DBHelper.AssingmentSelect, Assingment_View);
+            }
+            else if (Data_Tab_Control.SelectedIndex == 1)
+            {
+                DBHelper.FillData(DBHelper.EmployeeSelect, Employee_View);
+            }
+        }
+
+        private void Tasks_Combo_Box_DropDownOpened(object sender, EventArgs e)
+        {
+            Tasks_Combo_Box.ItemsSource = DBHelper.GetDataString("select Name from Tasks", "Name");
+        }
+
+        private void Empl_Combo_Box_DropDownOpened(object sender, EventArgs e)
+        {
+            Empl_Combo_Box.ItemsSource = DBHelper.GetDataString("select Name from Employee", "Name");
+        }
+
+        private void Employee_View_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsUpdatingEmp_Ass)
+            {
+                DBHelper.get_emoloyee_fields(Employee_View, Employee_Name_Textbox);
+            }
+        }
+
+        private void Assingment_View_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsUpdatingAssingment)
+            {
+                DBHelper.get_assingment_fields(Assingment_View, Tasks_Combo_Box, Empl_Combo_Box);
+            }
+        }
+
+        private void Data_Tab_Control_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsUpdatingAssingment || IsUpdatingEmp_Ass)
+            {
+                IsUpdatingEmp_Ass = false;
+                IsUpdatingAssingment = false;
+                unable_buttons();
+                Update_button.Content = "Изменить";
+            }
         }
     }
 }
